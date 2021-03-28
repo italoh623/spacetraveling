223ecce6567cf8dd42e4f93ecde09bd3339f360b
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-03-28 00:10:50 -0300

    Adding navigation in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import styles from './header.module.scss';
 
 export default function Header() {
@@ -5,7 +7,9 @@ export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo"/>
+        <Link href="/">
+          <img src="/images/logo.svg" alt="logo"/>
+        </Link>
       </div>
     </header>
   )
